e9b896504fc68d6437860469fa71864aeaf80f9e
unknown <redradishk@gmail.com>
2020-11-25 12:18:15 +0300

    fix repository

--- src/main/java/net/radish/crawler/repository/GoodUrlRepository.java
+++ src/main/java/net/radish/crawler/repository/GoodUrlRepository.java
@@ -1,7 +1,8 @@
 package net.radish.crawler.repository;
 
 import net.radish.crawler.model.Good;
+import net.radish.crawler.model.GoodUrl;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GoodUrlRepository extends JpaRepository<Good, Long> {
+public interface GoodUrlRepository extends JpaRepository<GoodUrl, Long> {
 }
--- src/main/java/net/radish/crawler/repository/ShopRepository.java
+++ src/main/java/net/radish/crawler/repository/ShopRepository.java
@@ -1,7 +1,8 @@
 package net.radish.crawler.repository;
 
 import net.radish.crawler.model.Good;
+import net.radish.crawler.model.Shop;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ShopRepository extends JpaRepository<Good, Long> {
+public interface ShopRepository extends JpaRepository<Shop, Long> {
 }
