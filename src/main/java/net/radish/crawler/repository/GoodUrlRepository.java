a013ff4a24ae987d1a3a4549ea779285f7f164b5
unknown <redradishk@gmail.com>
2020-11-25 12:15:09 +0300

    add repositories

--- src/main/java/net/radish/crawler/repository/GoodUrlRepository.java
+++ src/main/java/net/radish/crawler/repository/GoodUrlRepository.java
@@ -3,5 +3,5 @@ package net.radish.crawler.repository;
 import net.radish.crawler.model.Good;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GoodRepository extends JpaRepository<Good, Long> {
+public interface GoodUrlRepository extends JpaRepository<Good, Long> {
 }
