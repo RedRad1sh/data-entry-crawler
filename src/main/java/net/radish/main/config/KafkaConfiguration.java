a24d1105cb6c08c6be7aad22d005bce16a59d697
Nikitos <zmeunik@mail.ru>
2021-02-16 23:37:18 +0300

    kafka connecting add

--- src/main/java/net/radish/main/config/KafkaConfiguration.java
+++ src/main/java/net/radish/main/config/KafkaConfiguration.java
@@ -2,7 +2,9 @@ package net.radish.main.config;
 
 import net.radish.main.model.GoodUrl;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.security.plain.PlainLoginModule;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -16,20 +18,30 @@ import java.util.Map;
 @Configuration
 public class KafkaConfiguration {
 
+    @Value("${kafka.server}")
+    private String server;
+
+    @Value("${kafka.username}")
+    private String username = "";
+
+    @Value("${kafka.password}")
+    private String password = "";
+
     @Bean
     public ProducerFactory<String, GoodUrl> producerFactory() {
-        Map<String, Object> configProps = new HashMap<>();
-        configProps.put(
+        Map<String, Object> props = new HashMap<>();
+        props.put(
                 ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                "localhost:9092");
-        configProps.put(
+                server);
+        props.put(
                 ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
                 StringSerializer.class);
-        configProps.put(
+        props.put(
                 ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
                 JsonSerializer.class);
+        props.put("sasl.jaas.config", PlainLoginModule.class.getName() + " required username=\"" + username + "\" password=\"" + password + "\";");
 
-        return new DefaultKafkaProducerFactory<>(configProps);
+        return new DefaultKafkaProducerFactory<>(props);
     }
 
     @Bean
